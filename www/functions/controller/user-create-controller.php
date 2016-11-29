6d14869259e9338ebff8886c50bad47be80912df
Loustik <nicolas.lethellier@gmail.com>
2016-11-29 15:30:55 +0100

    fix sur les users

--- www/functions/controller/user-create-controller.php
+++ www/functions/controller/user-create-controller.php
@@ -1,4 +1,6 @@
 <?php
+	require_once('functions/str.php');
+	
 	if (isset($_POST['addUser'])){
 		$lastName = str_replace("'","", $_POST['inputUserLastName']);
 		$lastNameDB = strtolower(str_replace(" ","-", $lastName));
--- www/functions/controller/user-update-controller.php
+++ www/functions/controller/user-update-controller.php
@@ -1,4 +1,5 @@
 <?php
+	require_once('functions/str.php');
 
 	if (empty($commonError)){
 
