7e12190812f8ebe110ef77111248c5332ff54709
Loustik <nicolas.lethellier@gmail.com>
2016-11-30 15:35:12 +0100

    dix sur le nom de la table roles

--- www/functions/session/db-connect.php
+++ www/functions/session/db-connect.php
@@ -9,7 +9,7 @@ $tablename_ratcommune = "rat_com";		//liste des rattachements aux communes
 $tablename_ratmembre = "rat_membre";		//membres vis à vis des communes
 $tablename_ratdepartement = "rat_dpt";		//membres vis à vis du département
 $tablename_permission = "rbac_permissions";		//gestion des permissions
-$tablename_roles = "rbac_permissions";		//gestion des roles
+$tablename_roles = "rbac_roles";		//gestion des roles
 $link = mysqli_connect($hostname_dbprotect,$username_dbprotect,$password_dbprotect,$database_dbprotect) or die("Error " . mysqli_error($link));
-if (!mysqli_set_charset($link, "utf8")) {}
+if (!mysqli_set_charset($link, 'utf8')) { echo "charset not utf8";}
 ?>
