32806996b4e9184a8ef2471715179eb51f04a870
Loustik <nicolas.lethellier@gmail.com>
2016-12-02 18:32:39 +0100

    petit souci dans l'authentication

--- www/functions/dps/dps-authentication.php
+++ www/functions/dps/dps-authentication.php
@@ -1,15 +1,28 @@
 <?php 
 	if(isset($_GET['city'])){
-		if (empty($_GET['city']) || $currentUserSection == $_GET['city']) {
+		if (empty($_GET['city']) ) {
+			// User comes from its section's link
 			$rbac->enforce("ope-dps-view-own", $currentUserID);
 			$city=$currentUserSection; 
 		}
 		else {
-			$rbac->enforce("ope-dps-view-all", $currentUserID);
-			$city=$_GET['city'];
+			// User wants to view a specific city (maybe its own)
+			if ($currentUserSection == $_GET['city']){
+				// User explicitely wants to view its section's DPS
+				if (!$rbac->check("ope-dps-view-all", $currentUserID)) {
+					$rbac->enforce("ope-dps-view-own", $currentUserID);
+				}
+				$city=$currentUserSection; 
+			}
+			else {
+				// User wants to view another city's DPS
+				$rbac->enforce("ope-dps-view-all", $currentUserID);
+				$city=$_GET['city'];
+			}
 		}
 	}
 	else {
+		// User wants to view all DPS from the main link, or the 'ALL' filter
 		$rbac->enforce("ope-dps-view-all", $currentUserID); 
 	}
 ?>
