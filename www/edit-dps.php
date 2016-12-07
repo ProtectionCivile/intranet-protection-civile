5cf02fdd26a1d6a4cf502924f65eaf8fb6ce3172
Loustik <nicolas.lethellier@gmail.com>
2016-12-07 09:21:06 +0100

    centralisation des derniers JS

--- www/edit-dps.php
+++ www/edit-dps.php
@@ -827,94 +827,9 @@ if(file_exists($pathfiledemande)){$filedemande = true;}else{$filedemande = false
 		<?php }if ($_SESSION['privilege'] == "user") { ?>
   		<strong>En tant qu'utilisateur simple vous ne pouvez pas effectuer d'actions</strong> <?php }?>
 </div>
-<script type="text/javascript">
-    $(function () {
-        $("[rel='tooltip']").tooltip();
-    });
-	$(function () {
-		$('[data-toggle="popover"]').popover()
-	})
-</script>
+
 <script>
-		$('.upload-all').click(function(){
-			//submit all form
-			$('form').submit();
-		});
-
-		$('.cancel-all').click(function(){
-			//submit all form
-			$('form .cancel').click();
-		});
-
-		$(document).on('submit','.upload1',function(e){
-			e.preventDefault();
-			$form = $(this);
-			uploadImage($form);
-
-		});
 		
-		$(document).on('submit','.upload2',function(e){
-			e.preventDefault();
-			$form = $(this);
-			uploadImage($form);
-		});
-		$(document).on('submit','.upload3',function(e){
-			e.preventDefault();
-			$form = $(this);
-			uploadImage($form);
-		});
-		$(document).on('submit','.upload4',function(e){
-			e.preventDefault();
-			$form = $(this);
-			uploadImage($form);
-		});
-
-		function uploadImage($form){
-			$form.find('.progress-bar').removeClass('progress-bar-success')
-										.removeClass('progress-bar-danger');
-
-			var formdata = new FormData($form[0]); //formelement
-			var request = new XMLHttpRequest();
-
-			//progress event...
-			request.upload.addEventListener('progress',function(e){
-				var percent = Math.round(e.loaded/e.total * 100);
-				$form.find('.progress-bar').width(percent+'%').html(percent+'%');
-			});
-
-			//progress completed load event
-			request.addEventListener('load',function(e){
-				$form.find('.progress-bar').addClass('progress-bar-success').html('Veuillez patienter...');
-				//$form.find('.progress').removeClass('progress-striped');
-				
-				
-				
-			});
-
-			request.onreadystatechange = function(){
-				if(request.readyState == 4 && request.status == 200){
-					$form.find('.progress').removeClass('progress-striped');
-					$form.find('.progress-bar').html('Transfert Terminé !');
-					window.setTimeout(function(){location.reload()},2000);
-					
-				}
-			}
-			request.open('POST', 'functions/dps-documents-upload.php', true);
-			request.send(formdata);
-			
-			
-			
-
-			$form.on('click','.cancel',function(){
-				request.abort();
-
-				$form.find('.progress-bar')
-					.addClass('progress-bar-danger')
-					.removeClass('progress-bar-success')
-					.html('upload aborted...');
-			});
-
-		}
 </script>
 
 <script type="text/javascript">
@@ -931,6 +846,7 @@ if(file_exists($pathfiledemande)){$filedemande = true;}else{$filedemande = false
 </script>
 
 <script src='js/dps-compute-ris.js' type='text/javascript'></script>
+<script src='js/dps-doc-upload.js' type='text/javascript'></script>
 
 <?php require_once('components/footer.php'); ?>
 </body>
