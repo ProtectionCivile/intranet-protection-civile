5ab74b3770a715870479709a583d1a0177032fa0
Nicolas L <Loustik@users.noreply.github.com>
2017-07-25 11:44:22 +0200

    changement des glyphicons dans le OperationStatusIndicator

--- www/components/operation-status-indicator.php
+++ www/components/operation-status-indicator.php
@@ -1,27 +1,27 @@
 <?php
-	if (!empty($genericError)){ 
+	if (!empty($genericError)){
 	?>
 		<div class='alert alert-danger alert-dismissible' role='alert'>
 			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				<span aria-hidden="true">&times;</span>
 			</button>
-			<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+			<span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span>
  			<span class="sr-only">Error:</span>
   			<?php echo $genericError; ?>
 		</div>
-	
-	<?php 
+
+	<?php
 	} elseif (!empty($genericSuccess)){
 		?>
 		<div class='alert alert-success alert-dismissible' role='alert'>
 			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				<span aria-hidden="true">&times;</span>
 			</button>
-			<span class="glyphicon glyphicon-ok-sign" aria-hidden="true"></span>
+			<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
  			<span class="sr-only">Success:</span>
   			<?php echo $genericSuccess; ?>
 		</div>
-	
-	<?php 
+
+	<?php
 	}
 ?>
