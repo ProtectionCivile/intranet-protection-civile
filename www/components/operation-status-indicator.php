8ff90fa03c2b7cd18199aede1909eabb6a538700
Nicolas L <Loustik@users.noreply.github.com>
2017-07-25 15:57:26 +0200

    Chagement de la taille du logo dans OSIndicator

--- www/components/operation-status-indicator.php
+++ www/components/operation-status-indicator.php
@@ -5,7 +5,7 @@
 			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				<span aria-hidden="true">&times;</span>
 			</button>
-			<span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></span>
+			<span class="glyphicon glyphicon-warning-sign" aria-hidden="true" style="font-size:2em"></span>
  			<span class="sr-only">Error:</span>
   			<?php echo $genericError; ?>
 		</div>
@@ -17,7 +17,7 @@
 			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				<span aria-hidden="true">&times;</span>
 			</button>
-			<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+			<span class="glyphicon glyphicon-ok" aria-hidden="true" style="font-size:2em"></span>
  			<span class="sr-only">Success:</span>
   			<?php echo $genericSuccess; ?>
 		</div>
