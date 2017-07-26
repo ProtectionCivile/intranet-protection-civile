66d4078f99af45ae4ae8dcdda738dad938d2a81a
Nicolas L <Loustik@users.noreply.github.com>
2017-07-26 17:20:37 +0200

    Revert "le filtre est maintenant actif et amélioré"

--- www/components/filter/filter-dps-list-module.php
+++ www/components/filter/filter-dps-list-module.php
@@ -15,15 +15,11 @@
 		<h1 class="panel-title">Filtres</h1>
 	</div>
 	<div class="panel-body" aria-expanded='true'>
-
+		
 		<div class='panel-filters in' aria-expanded='true'>
 			<p>
 				<!-- Filter on the city holding the DPS -->
-				<?php
-				if ($rbac->check("ope-dps-view-all", $currentUserID)) {
-					include_once('components/filter/parts/filter-city-display.php');
-				}
-				?>
+				<?php include_once('components/filter/parts/filter-city-display.php'); ?>
 			</p>
 		</div>
 
