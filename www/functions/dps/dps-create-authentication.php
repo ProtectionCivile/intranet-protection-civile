35fe7ade66982baee4595ee17d7e3cdb258bba01
Loustik <nicolas.lethellier@gmail.com>
2017-08-04 13:02:42 +0200

    pb de droits

--- www/functions/dps/dps-create-authentication.php
+++ www/functions/dps/dps-create-authentication.php
@@ -2,6 +2,9 @@
 	if ($city == $currentUserSection && !$rbac->check("ope-dps-create-all", $currentUserID)) {
 		$rbac->enforce("ope-dps-create-own", $currentUserID);
 	}
+	elseif ($city == '0' && !$rbac->check("ope-dps-create-all", $currentUserID)) {
+		$rbac->enforce("ope-dps-create-dept", $currentUserID);
+	}
 	else {
 		$rbac->enforce("ope-dps-create-all", $currentUserID);
 	}
--- www/functions/dps/dps-view-authentication.php
+++ www/functions/dps/dps-view-authentication.php
@@ -2,6 +2,9 @@
 	if ($dps['section'] == $currentUserSection && !$rbac->check("ope-dps-view-all", $currentUserID)) {
 		$rbac->enforce("ope-dps-view-own", $currentUserID);
 	}
+	elseif ($dps['section'] == '0' && !$rbac->check("ope-dps-view-all", $currentUserID)) {
+		$rbac->enforce("ope-dps-view-dept", $currentUserID);
+	}
 	else {
 		$rbac->enforce("ope-dps-view-all", $currentUserID);
 	}
