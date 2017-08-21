fa1567d737ee456cf26fa62260cc7c20e235f785
Nicolas L <Loustik@users.noreply.github.com>
2017-08-21 20:02:58 +0200

    Exemple de réalisation

---
+++ www/functions/mail/write-dps-mail.php
@@ -0,0 +1,40 @@
+<?php
+require('functions/mail/Mail.php');
+require_once('functions/mail/mail-functions.php');
+require_once('functions/dps/dps-select-parameters-computation.php');
+
+
+// Paramètre d'entrée : l'action désrée (validation locale? refus DDO?)
+$action = 'validate-local'; // TODO retirer cet exemple
+
+
+if ($action = 'validate-local') {
+
+}
+
+
+if ($action = 'cancel-local') {
+
+}
+
+
+if ($action = 'cancel-ddo') {
+
+}
+
+
+if ($action = 'wait') {
+
+}
+
+
+if ($action = 'accept-ddo') {
+
+}
+
+
+if ($action = 'reject-ddo') {
+
+}
+
+?>
