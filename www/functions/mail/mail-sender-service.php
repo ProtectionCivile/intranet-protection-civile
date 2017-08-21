45f8858f8cd232bc2533f001e238fa165425018e
Nicolas L <Loustik@users.noreply.github.com>
2017-08-21 20:22:03 +0200

    squelette du mail-sener-service

---
+++ www/functions/mail/mail-sender-service.php
@@ -0,0 +1,7 @@
+<?php
+
+// Appelé régulièrement par le CRON, dépile et envoie les mails en attente de la base de données.
+// Dès qu'ils sont traités, il positionne la date d'envoi à la date du jour
+
+
+?>
