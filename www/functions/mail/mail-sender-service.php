aafdb226add369b585362d737a564276a903306f
Nicolas L <Loustik@users.noreply.github.com>
2017-08-22 01:21:01 +0200

    service d'envoi de mail (À COMPLÉTER)

--- www/functions/mail/mail-sender-service.php
+++ www/functions/mail/mail-sender-service.php
@@ -1,7 +1,38 @@
 <?php
-
 // Appelé régulièrement par le CRON, dépile et envoie les mails en attente de la base de données.
 // Dès qu'ils sont traités, il positionne la date d'envoi à la date du jour
+
+require_once('functions/session/db-connect.php');
+
+
+$sql = "SELECT `id`, `sender`, `recipients`, `cc_recipients`, `subject`, `message`, `attachements` FROM $tablename_mail WHERE `date_sent` IS NULL ORDER BY id ASC";
+foreach  ($db_link->query($sql) as $row) {
+  // Récupération des informations du Mail
+  $id = $row['id'];
+  $sender = $row['sender'];
+  $recipients = $row['recipients'];
+  $cc_recipients = $row['cc_recipients'];
+  $subject = $row['subject'];
+  $message = $row['message'];
+  $attachementFilePaths = explode(',', $row['attachements']);
+
+  // TODO Ici il faut envoyer le mail
+  // Si on peut, récupérer le résultat de l'envoi dans une variable $sent.
+
+  $sent = TRUE; // A faire varier selon si oui ou non le mail a été envoyé.
+
+  // Positionnement de la date d'envoi à aujourd'hui
+  if ($sent) {
+    $today = date("Y-m-d H:i:s");
+    $sql = "UPDATE `".$tablename_mail."` SET `date_sent`='".$today."' WHERE `id`='".$id."' ";
+    if ($db_link->query($sql) === TRUE) {
+      echo "OK<br />";
+    }
+    else {
+      echo "Failure (".$db_link->error.")<br />"; // Ou bien renvoyer une erreur 500 ?
+    }
+  }
+}
 
 
 ?>
