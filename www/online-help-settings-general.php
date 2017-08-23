21042aba622310667adc2b83564889db76260ce9
Nicolas L <Loustik@users.noreply.github.com>
2017-08-23 15:51:45 +0200

    table plus jolie

--- www/online-help-settings-general.php
+++ www/online-help-settings-general.php
@@ -26,12 +26,15 @@
 	<h3 class='text-primary'>Droits d'accès</h3>
 	<p>Il existe une permission pour les consulter, une autre pour les modifier.</p>
 
-	<h3 class='text-primary'>Paramètres par défaut, indispensables à l'application</h3>
+	<h3 class='text-primary'>Utilisation</h3>
+	<p>Les paramètres et les valeurs sont directement éditables par le site.</p>
 	<div class='alert alert-danger' role='alert'><span class='glyphicon glyphicon-warning-sign'></span> Sans ces paramètres, l'application ne peut pas fonctionner correctement. À manipuler avec précaution, parce qu'une modification non-réfléchie peut entraîner des coupures de service.</div>
 	<p>Les valeurs des paramètres peuvent néanmoins être modifiées.</p>
 
+	<h3 class='text-primary'>Paramètres par défaut, indispensables à l'application</h3>
+
 	<div class='table-responsive'>
-		<table class='table table-condensed'>
+		<table class='table table-striped table-hover'>
 			<thead>
 				<tr>
 					<th>Nom</th>
@@ -41,44 +44,44 @@
 			</thead>
 			<tbody>
 				<tr>
-					<td>application-header-name</td>
-					<td>Extranet PC92</td>
-					<td>Le nom qui apparaît en haut à gauche de la barre de navigation</td>
+					<td><samp>application-header-name</samp></td>
+					<td><samp>Extranet PC92</samp></td>
+					<td class='text-muted'>Le nom qui apparaît en haut à gauche de la barre de navigation</td>
 				</tr>
 				<tr>
-					<td>dps-doc-suffix-convention</td>
-					<td>CONV</td>
-					<td>Suffixe des noms fichier de convention de DPS, pour l'upload et la recherche. Par exemple: 92-17-COU-021-CONV.pdf</td>
+					<td><samp>dps-doc-suffix-convention</samp></td>
+					<td><samp>CONV</samp></td>
+					<td class='text-muted'>Suffixe des noms fichier de convention de DPS, pour l'upload et la recherche. Par exemple: 92-17-COU-021-CONV.pdf</td>
 				</tr>
 				<tr>
-					<td>dps-doc-suffix-risk</td>
-					<td>RISK</td>
-					<td>Suffixe des noms fichier de grille de risques de DPS</td>
+					<td><samp>dps-doc-suffix-risk</samp></td>
+					<td><samp>RISK</samp></td>
+					<td class='text-muted'>Suffixe des noms fichier de grille de risques de DPS</td>
 				</tr>
 				<tr>
-					<td>dps-doc-suffix-demande</td>
-					<td>DEM</td>
-					<td>Suffixe des noms fichier de demande de DPS rempli par l'organisateur</td>
+					<td><samp>dps-doc-suffix-demande</samp></td>
+					<td><samp>DEM</samp></td>
+					<td class='text-muted'>Suffixe des noms fichier de demande de DPS rempli par l'organisateur</td>
 				</tr>
 				<tr>
-					<td>dps-doc-suffix-declaration</td>
-					<td>DECL</td>
-					<td>Suffixe des noms fichier de déclaration en préfecture pour les DPS</td>
+					<td><samp>dps-doc-suffix-declaration</samp></td>
+					<td><samp>DECL</samp></td>
+					<td class='text-muted'>Suffixe des noms fichier de déclaration en préfecture pour les DPS</td>
 				</tr>
 				<tr>
-					<td>eprotec-event-url</td>
-					<td>https://franceprotectioncivile.org/evenement_display.php?evenement=EVENTID</td>
-					<td>Lien par défaut pour un évènement eProtec, utilisé pour les déclarations de DPS. &lt;EVENTID&gt; sera remplacé par le numéro d'évènement</td>
+					<td><samp>eprotec-event-url</samp></td>
+					<td><samp>https://franceprotectioncivile.org/evenement_display.php?evenement=EVENTID</samp></td>
+					<td class='text-muted'>Lien par défaut pour un évènement eProtec, utilisé pour les déclarations de DPS. &lt;EVENTID&gt; sera remplacé par le numéro d'évènement</td>
 				</tr>
 				<tr>
-					<td>mail-signature-ddo</td>
-					<td>&lt;strong&gt;Le Directeur Départemental des Opérations&lt;/strong&gt;&lt;br /&gt;operationnel@protectioncivile92.org&lt;br /&gt;06.74.95.31.75</td>
-					<td>Signature par défaut du DDO pour les mails envoyés</td>
+					<td><samp>mail-signature-ddo</samp></td>
+					<td><samp>&lt;strong&gt;Le Directeur Départemental des Opérations&lt;/strong&gt;&lt;br /&gt;operationnel@protectioncivile92.org&lt;br /&gt;06.74.95.31.75</samp></td>
+					<td class='text-muted'>Signature par défaut du DDO pour les mails envoyés</td>
 				</tr>
 				<tr>
-					<td>mail-signature-dlo</td>
-					<td>&lt;strong&gt;Le Directeur Local des Opérations&lt;/strong&gt;</td>
-					<td>Signature par défaut du DLO pour les mails envoyés</td>
+					<td><samp>mail-signature-dlo</samp></td>
+					<td><samp>&lt;strong&gt;Le Directeur Local des Opérations&lt;/strong&gt;</samp></td>
+					<td class='text-muted'>Signature par défaut du DLO pour les mails envoyés</td>
 				</tr>
 			</tbody>
 		</table>
