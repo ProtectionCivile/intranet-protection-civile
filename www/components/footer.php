0250bef8b2c005043b5d0971839045be7ef7d126
Nicolas L <Loustik@users.noreply.github.com>
2017-08-24 11:36:25 +0200

    Footer

--- www/components/footer.php
+++ www/components/footer.php
@@ -1,4 +1,9 @@
+<footer class='footer'>
+  <div classs='container'>
+    <p class='text-center text-muted'><small>Protection Civile des Hauts-de-Seine. Tous droits réservés.</small></p>
+  </div>
+</footer>
+
 
 <script src='js/popover-and-tooltip-application.js' type='text/javascript'></script>
 <script src='js/jquery-validator-settings.js' type='text/javascript'></script>
-
