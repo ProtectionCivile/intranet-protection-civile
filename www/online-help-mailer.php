67f32aad79b9e91de7af49a4a81c30ff53c63a80
Nicolas L <Loustik@users.noreply.github.com>
2017-08-24 17:05:28 +0200

    aide mailer : préparation

---
+++ www/online-help-mailer.php
@@ -0,0 +1,39 @@
+<?php require_once('functions/session/security.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Aide en ligne</title>
+	<?php require_once('components/common-html-head-parameters.php'); ?>
+</head>
+<body>
+<?php include('components/header.php'); ?>
+
+
+<ol class="breadcrumb">
+	<li><a href="/">Accueil</a></li>
+	<li><a href="online-help.php">Aide en ligne</a></li>
+	<li class="active">Système d'envoi d'e-mails</li>
+</ol>
+
+<div class="container">
+
+	<div class="page-header">
+		<h1>Aide en ligne <small>Système d'envoi d'e-mails</small></h1>
+	</div>
+
+	<p class='lead'>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA.</p>
+
+	<br />
+	<h3 class='text-primary'>Fonctionnement</h3>
+	<p>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+
+	<br />
+	<h3 class='text-primary'>Administration</h3>
+	<p>Un icone est dispo pour les admins en haut à droite</p>
+
+</div>
+
+<?php include('components/footer.php'); ?>
+
+</body>
+</html>
