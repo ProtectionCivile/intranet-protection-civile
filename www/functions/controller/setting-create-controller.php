bdb45a927d9fc7d809b95096201d1fb323944be0
Nicolas L <Loustik@users.noreply.github.com>
2017-08-29 02:21:58 +0200

    Setting

--- www/functions/controller/setting-create-controller.php
+++ www/functions/controller/setting-create-controller.php
@@ -1,19 +1,28 @@
 <?php
-	
-	//Authentication 
+
+	//Authentication
 	$rbac->enforce("admin-settings-update", $currentUserID);
 
 	if (isset($_POST['addSetting'])){
-		$name = str_replace("'","", $_POST['name']);
-		$value = str_replace("'","", $_POST['value']);
+		$name = $_POST['name'];
+		$value = $_POST['value'];
 
-		if($name == ""){
-			$genericError = "Le nom est obligatoire";
-			$createErrorName = "Le nom est obligatoire";
+		$missingValues = 0;
+
+		if(isNullOrEmpty($name)){
+			$missingValues++;
+			$name_error = "Le nom est obligatoire";
 		}
-		
+
+		if ($missingValues != "0" ) {
+			if (!isNullOrEmpty($genericError)){
+				$genericError = $genericError.'<br />';
+			}
+				$genericError = $genericError.'Il y a '.$missingValues.' champs non-renseignés';
+		}
+
 		if (empty($genericError)){
-			$verif_query = "SELECT * FROM $tablename_settings_general WHERE name='$name'"; 
+			$verif_query = "SELECT * FROM $tablename_settings_general WHERE name='$name'";
 			$verif = mysqli_query($db_link, $verif_query);
 			if(!$verif){
 				trigger_error("Erreur lors de la consultation" . mysqli_error($db_link));
@@ -24,7 +33,8 @@
 			}
 			else {
 				$query = mysqli_prepare($db_link, "INSERT INTO $tablename_settings_general (name, value) VALUES (?, ?)");
-				mysqli_stmt_bind_param($query, "ss", $name, $value);
+				mysqli_stmt_bind_param($query, "ss", mysqli_real_escape_string($db_link, $name), mysqli_real_escape_string($db_link, $value));
+
 				if(!mysqli_stmt_execute($query)){
 					$genericError = "Impossible d'ajouter le paramètre '".$name."'";
 					trigger_error($genericError . mysqli_error($db_link));
