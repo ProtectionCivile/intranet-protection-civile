4c0f986b129cb18610bdf0157f4c7fe2abdf5c61
Nicolas L <Loustik@users.noreply.github.com>
2017-08-30 17:14:50 +0200

    filtrage par statut

--- www/components/filter/parts/filter-common-display.php
+++ www/components/filter/parts/filter-common-display.php
@@ -7,7 +7,7 @@ if (isset($_POST['hasSubmittedFilterParameters'])) {
 	$_SESSION['datebegin'] = $_POST['formdatebegin'];
 	$_SESSION['dateend'] = $_POST['formdateend'];
 	$_SESSION['filtered_section'] = $_POST['formfilteredsection'];
-	// $_SESSION['nb_elements_per_page'] = $_POST['formnbelementsperpage'];
+	$_SESSION['dps_status'] = $_POST['formstatus'];
 }
 ?>
 
@@ -15,7 +15,7 @@ if (isset($_POST['hasSubmittedFilterParameters'])) {
 <form role='form' id='formfilter' action='<?php echo $base_url; ?>' method='post'>
 	<input type='hidden' name='hasSubmittedFilterParameters' id='hasSubmittedFilterParameters' value='1'>
 	<input type='hidden' name='formfilteredsection' id='formfilteredsection' value='<?php echo $_SESSION['filtered_section']; ?>'>
-	<input type='hidden' name='formstatus' id='formstatus' value='<?php echo $status; ?>'>
+	<input type='hidden' name='formstatus' id='formstatus' value='<?php echo $_SESSION['dps_status']; ?>'>
 	<input type='hidden' name='formdatebegin' id='formdatebegin' value='<?php echo $_SESSION['datebegin']; ?>'>
 	<input type='hidden' name='formdateend' id='formdateend' value='<?php echo $_SESSION['dateend']; ?>'>
 	<input type='hidden' name='formcurrentpage' id='formcurrentpage' value='<?php echo $_SESSION['current_page']; ?>'>
