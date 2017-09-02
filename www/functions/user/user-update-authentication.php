99177f6b1fe828c0ecf9acc3ef3e7de3a9c97ee5
Nicolas L <Loustik@users.noreply.github.com>
2017-09-02 21:51:13 +0200

    user auth

--- www/functions/user/user-update-authentication.php
+++ www/functions/user/user-update-authentication.php
@@ -1,1 +1,9 @@
-<?php $rbac->enforce("admin-users-update", $currentUserID); ?>
+<?php
+
+if (isset($_POST['updateUser']) { // If an admin wishes to update a user
+  $rbac->enforce("admin-users-update", $currentUserID);
+}
+else { // If user only wants to update its own password
+  // no permission required.
+}
+?>
