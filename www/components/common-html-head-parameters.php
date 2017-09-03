88d78798c5b3c991c38b06c95753e8efef45f10e
Loustik <nicolas.lethellier@gmail.com>
2017-09-03 14:55:24 +0200

    html

--- www/components/common-html-head-parameters.php
+++ www/components/common-html-head-parameters.php
@@ -1,4 +1,4 @@
-<meta http-equiv="Content-Type" content="text/html">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta charset="UTF-8">
 <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="all" title="no title" charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
