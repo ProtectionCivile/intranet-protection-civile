4b3f763a590c8fbc9f39aa828a8be3cf3042cf09
Nicolas L <Loustik@users.noreply.github.com>
2018-01-14 02:26:40 +0100

    status et section. a vérifier le status all / atraiter

--- www/components/filter/parts/filter-status-computation.php
+++ www/components/filter/parts/filter-status-computation.php
@@ -1,9 +1,9 @@
 <?php
-	if(isset($_SESSION['dps_status']) && !empty($_SESSION['dps_status']) ){
+if (isset($_GET['atraiter'])) {
+	$status="atraiter";
+}
+elseif(isset($_SESSION['dps_status']) && !empty($_SESSION['dps_status']) ){
 		$status=$_SESSION['dps_status'];
-	}
-	elseif (isset($_GET['atraiter'])) {
-		$status="atraiter";
 	}
 	else {
 		$status="*";
